a08193aa038de1aba0b5a464051c15b0473a8e77
Anastacio Gianareas <tacho87@hotmail.com>
2017-10-06 17:44:12 -0500

    added scrollview

--- src/components/home/homeScreen.js
+++ src/components/home/homeScreen.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View, Button, ScrollView, Image } from 'react-native';
 
 
 export default class HomeScreen extends React.Component {
@@ -11,8 +11,69 @@ export default class HomeScreen extends React.Component {
     render() {
         return (
             <View style={styles.container}>
-                <Text style={styles.baseText}>Home Screen</Text>
-                <Button onPress={() => this.props.navigation.navigate('Search')} title="Search For..." />
+                <ScrollView style={styles.container}>
+                    <View style={styles.boxLarge}>
+                        <Image
+                            style={{ height: "100%", width: "100%" }}
+                            source={{ uri: 'https://s3.amazonaws.com/fotos-horeca/felipemotta/MIGPS0471.jpg' }} />
+                    </View>
+                    <ScrollView horizontal>
+                        <View style={styles.boxSmall}>
+                            <Image
+                                style={{ height: "100%", width: "100%" }}
+                                source={{ uri: 'https://s3.amazonaws.com/fotos-horeca/felipemotta/MIGPS0471.jpg' }} />
+                        </View>
+                        <View style={styles.boxSmall}>
+                            <Image
+                                style={{ height: "100%", width: "100%" }}
+                                source={{ uri: 'https://s3.amazonaws.com/fotos-horeca/felipemotta/MIGPS0191.jpg' }} />
+                        </View>
+                        <View style={styles.boxSmall}>
+                            <Image
+                                style={{ height: "100%", width: "100%" }}
+                                source={{ uri: 'https://s3.amazonaws.com/fotos-horeca/globalbrands/2951.jpg' }} />
+                        </View>
+                        <View style={styles.boxSmall}>
+                            <Image
+                                style={{ height: "100%", width: "100%" }}
+                                source={{ uri: 'https://s3.amazonaws.com/fotos-horeca/globalbrands/2953.jpg' }} />
+                        </View>
+                        <View style={styles.boxSmall}>
+                            <Image
+                                style={{ height: "100%", width: "100%" }}
+                                source={{ uri: 'https://s3.amazonaws.com/fotos-horeca/felipemotta/MIGPS2133.jpg' }} />
+                        </View>
+
+                    </ScrollView>
+                    <View style={styles.boxLarge}>
+                        <Image
+                            style={{ height: "100%", width: "100%" }}
+                            source={{ uri: 'https://s3.amazonaws.com/fotos-horeca/globalbrands/1366.jpg' }} />
+                    </View>
+                    <ScrollView horizontal>
+                        <View style={styles.boxSmall}>
+                            <Image
+                                style={{ height: "100%", width: "100%" }}
+                                source={{ uri: 'https://s3.amazonaws.com/fotos-horeca/globalbrands/7520.jpg' }} />
+                        </View>
+                        <View style={styles.boxSmall}>
+                            <Image
+                                style={{ height: "100%", width: "100%" }}
+                                source={{ uri: 'https://s3.amazonaws.com/fotos-horeca/felipemotta/MIGPS2181.jpg' }} />
+                        </View>
+                        <View style={styles.boxSmall}>
+                            <Image
+                                style={{ height: "100%", width: "100%" }}
+                                source={{ uri: 'https://s3.amazonaws.com/fotos-horeca/felipemotta/MIGPS2122.jpg' }} />
+                        </View>
+
+                    </ScrollView>
+                    <View style={styles.boxLarge}>
+                        <Image
+                            style={{ height: "100%", width: "100%" }}
+                            source={{ uri: 'https://s3.amazonaws.com/fotos-horeca/felipemotta/MIGPS0502.jpg' }} />
+                    </View>
+                </ScrollView>
             </View>
         );
     }
@@ -21,12 +82,19 @@ export default class HomeScreen extends React.Component {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center'
     },
-    baseText: {
-        color: 'gray',
-        textAlign: "left",
-        fontSize: 18
-    }
-});
+    boxSmall: {
+        width: 200,
+        height: 200,
+        marginBottom: 10,
+        marginRight: 10,
+        backgroundColor: 'skyblue',
+    },
+    boxLarge: {
+        width: "100%",
+        height: 600,
+        marginBottom: 10,
+        marginRight: 10,
+        backgroundColor: 'steelblue',
+    },
+})
